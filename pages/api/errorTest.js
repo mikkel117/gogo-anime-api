4b7b779ea647f3550873b33f2404fba4c5ac3ef5
rumbo <rumbo.9370@gmail.com>
2023-04-26 10:45:01 +0200

    update error test

--- pages/api/errorTest.js
+++ pages/api/errorTest.js
@@ -2,7 +2,18 @@ const cheerio = require("cheerio");
 import axios from "axios";
 
 export default async (req, res) => {
-  return res.status(404).json({
-    error: "error",
-  });
+  try {
+    const { data } = await axios.get("https://xkcd.c");
+    const $ = cheerio.load(data);
+    const title = $("#ctitle").text();
+    const lastScraped = new Date().toISOString();
+    return res.status(200).json({
+      title: title,
+      lastScraped: lastScraped,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      error: e.message,
+    });
+  }
 };
